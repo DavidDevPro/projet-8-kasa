84a91ffe29981a68811e4992115f4afab6c8f494
David Changea <14ir155@gmail.com>
2024-01-26 00:16:48 +0100

    banner à propos terminé

---
+++ src/components/AboutBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import aboutBanner from "../assets/img/aboutBanner.png";
+
+const AboutBanner = () => {
+  return (
+    <div className="aboutBanner">
+      <img src={aboutBanner} alt="bannière à propos" />
+    </div>
+  );
+};
+
+export default AboutBanner;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import AboutBanner from "../components/AboutBanner";
 
 const About = () => {
   return (
     <div>
       <Header />
+      <AboutBanner />
     </div>
   );
 };
