5b014f6745fed541e59979744207cf30ec8b53b1
David Changea <14ir155@gmail.com>
2024-01-25 18:31:20 +0100

    mise en place de sass, du routing sur app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Locations from "./pages/Locations";
+import Error404 from "./pages/Error404";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
