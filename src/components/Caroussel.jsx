1bc88e90b9e9b27bc54f1ab2548e46768d6fc4c3
David Changea <14ir155@gmail.com>
2024-02-02 17:13:02 +0100

    test caroussel v1

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import arrowRight from "../assets/img/arrowRight.svg";
+import arrowLeft from "../assets/img/arrowLeft.svg";
 
-const Caroussel = () => {
-  return <div className="caroussel"></div>;
+const Caroussel = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentSlide = slides[currentIndex];
+  const numSlides = slides.length;
+
+  const handleIndexChange = (increment) => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex + increment + numSlides) % numSlides
+    );
+  };
+
+  const shouldShowArrows = numSlides > 1;
+  const shouldShowIndicator = numSlides > 1;
+
+  return (
+    <div className="caroussel">
+      <img
+        className="slider"
+        src={currentSlide}
+        alt={`Slide ${currentIndex + 1}`}
+      />
+      {shouldShowArrows && (
+        <>
+          <img
+            className="arrowRight"
+            src={arrowRight}
+            alt="Flèche Droite"
+            onClick={() => handleIndexChange(1)}
+          />
+          <img
+            className="arrowLeft"
+            src={arrowLeft}
+            alt="Flèche Gauche"
+            onClick={() => handleIndexChange(-1)}
+          />
+        </>
+      )}
+      {shouldShowIndicator && (
+        <div className="slide-indicator">{`${
+          currentIndex + 1
+        }/${numSlides}`}</div>
+      )}
+    </div>
+  );
 };
 
 export default Caroussel;
