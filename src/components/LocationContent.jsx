3165c0db63d215bcddcbf3e9ec35b44fd727e15d
David Changea <14ir155@gmail.com>
2024-02-05 00:26:02 +0100

    ajout animation page d'accueil et caroussel

--- src/components/LocationContent.jsx
+++ src/components/LocationContent.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import locations from "../data/locations.json";
 import Caroussel from "./Caroussel.jsx";
 import Tags from "./Tags.jsx";
@@ -8,47 +8,56 @@ import DropList from "./DropList.jsx";
 
 const LocationContent = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const location = locations.find((item) => item.id === id);
+
+  useEffect(() => {
+    if (!location) {
+      navigate("/error404");
+    }
+  }, [id, location, navigate]);
+  if (!location) {
+    return null;
+  }
 
   console.log(id);
   return (
-    
-      <div className="locationContent">
-        <Caroussel slides={location.pictures} />
-        <div className="topContainer">
-          <div className="leftContainer">
-            <h2>{location.title}</h2>
-            <h3>{location.location}</h3>
-            <div className="tagContainer">
-              {location.tags.map((tag, index) => (
-                <Tags key={index} text={tag} />
-              ))}
-            </div>
-          </div>
-          <div className="rightContainer">
-            <div className="pictureContainer">
-              <h3>{location.host.name}</h3>
-              <img
-                src={location.host.picture}
-                alt={"photo de" + location.host.name}
-              />
-            </div>
-            <div className="rating">
-              <Rating rating={parseInt(location.rating)} />
-            </div>
+    <div className="locationContent">
+      <Caroussel slides={location.pictures} />
+      <div className="topContainer">
+        <div className="leftContainer">
+          <h2>{location.title}</h2>
+          <h3>{location.location}</h3>
+          <div className="tagContainer">
+            {location.tags.map((tag, index) => (
+              <Tags key={index} text={tag} />
+            ))}
           </div>
         </div>
-        <div className="botomContainer">
-          <DropList title="Déscription">{location.description}</DropList>
-          <DropList title="équipement">
-            <ul>
-              {location.equipements.map((option, index) => (
-                <li key={index}>{option}</li>
-              ))}
-            </ul>
-          </DropList>
+        <div className="rightContainer">
+          <div className="pictureContainer">
+            <h3>{location.host.name}</h3>
+            <img
+              src={location.host.picture}
+              alt={"photo de" + location.host.name}
+            />
+          </div>
+          <div className="rating">
+            <Rating rating={parseInt(location.rating)} />
+          </div>
         </div>
       </div>
+      <div className="botomContainer">
+        <DropList title="Déscription">{location.description}</DropList>
+        <DropList title="équipement">
+          <ul>
+            {location.equipements.map((option, index) => (
+              <li key={index}>{option}</li>
+            ))}
+          </ul>
+        </DropList>
+      </div>
+    </div>
   );
 };
 
